542965e64e3a9a24c62e156232d0bb29b19f498b
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-18 16:44:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
   if speed<=39 or speed>=61
-    return "false"  
+    return true 
   else 
-    return "true" 
+    return false 
   end
 end
 unsafe?(69)
 
 def not_safe?(speed)
-	speed>=40 && speed<=60 ?: false : true 
+	speed>=39 && speed<=61 ?: false : true 
 end
 not_safe?(55)
